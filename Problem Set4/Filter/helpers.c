0462627ab55620a162ae61ac5e6bd0c9c0953399
metryczka <78020522+metryczka@users.noreply.github.com>
2021-08-09 11:57:05 +0200

    Update helpers.c
    Sepia filter added

--- Problem Set4/Filter/helpers.c
+++ Problem Set4/Filter/helpers.c
@@ -20,6 +20,43 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int red = image[i][j].rgbtRed;
+            int green = image[i][j].rgbtGreen;
+            int blue = image[i][j].rgbtBlue;
+            int red2 = round(.393 * red + .769 * green + .189 * blue);
+            int green2 = round(.349 * red + .686 * green + .168 * blue);
+            int blue2 = round(.272 * red + .534 * green + .131 * blue);
+            if (red2 > 255)
+            {
+                image[i][j].rgbtRed = 255;
+            }
+            else
+            {
+                image[i][j].rgbtRed = red2;
+            }
+            if (green2 > 255)
+            {
+                image[i][j].rgbtGreen = 255;
+            }
+            else
+            {
+                image[i][j].rgbtGreen = green2;
+            }
+            if (blue2 > 255)
+            {
+                image[i][j].rgbtBlue = 255;
+            }
+
+            else
+            {
+                image[i][j].rgbtBlue = blue2;
+            }
+        }
+    }
     return;
 }
 
